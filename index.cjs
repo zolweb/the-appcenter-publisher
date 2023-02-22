79623fe19aa0d662ea6f1f57e5224a9cc7f2672d
Philippine Cluniat <philippine.cluniat@zol.fr>
2023-02-22 15:24:13 +0100

    [+] add ci mode

--- index.cjs
+++ index.cjs
@@ -3,7 +3,7 @@
 /* eslint-disable no-console */
 // Imports
 const { prompt } = require('enquirer');
-const { validateProjectConfig, getConfigObject } = require('./helpers/commonHelpers');
+const { validateProjectConfig, getConfigObject, printErrorConsoleMessage} = require('./helpers/commonHelpers');
 const { manageGitFlow, manageGitBranches } = require('./helpers/gitHelpers');
 const { triggerAppCenterBuild, createAppCenterDistributionGroups, updateAppCenterBranchConfig } = require('./helpers/appCenterHelpers');
 
@@ -12,33 +12,43 @@ const [, , ...args] = process.argv;
 const SCRIPT_PARAMS = {
   INIT_CONFIG: '--init-config',
   UPDATE_CONFIG: '--update-config',
+  CI_MODE: '--ci'
 };
+
+const PLATFORMS = ['ios', 'android'];
+const ENVIRONMENTS = ['staging', 'pre-prod', 'prod'];
 
 const deployPromptQuestions = [
   {
     type: 'multiselect',
     name: 'platform',
     message: 'Select the platform(s) for your build',
-    choices: ['ios', 'android'],
+    choices: PLATFORMS,
     validate: (choices) => (choices.length === 0 ? 'You need to choose at lease one platform to start building' : true),
   },
   {
     type: 'select',
     name: 'branch',
     message: 'Select the environment you want your app built for',
-    choices: ['staging', 'pre-prod', 'prod'],
+    choices: ENVIRONMENTS,
   },
 ];
 
-const triggerDeployScript = async () => {
+const triggerDeployScript = async ({isCi, platformParam, branchParam}) => {
   const CONFIG = getConfigObject();
+  let platform = platformParam;
+  let branch = branchParam;
   try {
+    if(!isCi) {
     // Get inputs from user
-    const { platform, branch } = await prompt(deployPromptQuestions);
+    const userResponse = await prompt(deployPromptQuestions);
+    platform = userResponse.platform;
+    branch = userResponse.branch;
+    }
     // Run all the git commands to manage the versionning
     await manageGitFlow(branch, CONFIG);
     // Trigger AppCenter build via API call
-    triggerAppCenterBuild(platform, branch, CONFIG);
+    triggerAppCenterBuild(platform, branch);
   } catch (error) {
     console.error(error);
     process.exit(1);
@@ -56,18 +66,47 @@ const triggerInitConfigScript = async () => {
 
 const triggerUpdateConfigScript = () => { };
 
+const validateCIParams = ({platform, env}) => {
+  if(platform && !PLATFORMS?.includes(platform)) {
+    printErrorConsoleMessage(`platform param is incorrect, possible values are : ${PLATFORMS?.join(' or ')}. Leave empty for both`);
+    process.exit(1);
+  }
+  if(env && !ENVIRONMENTS?.includes(env)) {
+    printErrorConsoleMessage(`env param is incorrect, possible values are : ${ENVIRONMENTS?.join(' or ')}.`);
+    process.exit(1);
+  }
+}
+
 async function startScript() {
-  // First need to check if poject config file is valid
+  // First need to check if project config file is valid
   validateProjectConfig();
   // Check if user specified arguments
   const isInitConfig = args.includes(SCRIPT_PARAMS.INIT_CONFIG);
   const isUpdateConfig = args.includes(SCRIPT_PARAMS.UPDATE_CONFIG);
+  const isCI = args.includes(SCRIPT_PARAMS.CI_MODE);
 
-  if (isInitConfig) return triggerInitConfigScript();
+  if (isInitConfig) {
+    return triggerInitConfigScript();
+  }
 
-  if (isUpdateConfig) return triggerUpdateConfigScript();
+  if (isUpdateConfig) {
+    return triggerUpdateConfigScript();
+  }
 
-  return triggerDeployScript();
+  if (isCI) {
+    const defaultPlatform = ['android', 'ios'];
+    const defaultEnv = 'staging';
+    const platform = args.find((item) => item?.includes('platform'))?.split(':')?.pop();
+    const env = args.find((item) => item?.includes('env'))?.split(':')?.pop();
+    validateCIParams({platform, env});
+    return triggerDeployScript({
+      isCi: true,
+      platformParam: platform ? [platform] : defaultPlatform,
+      branchParam: env || defaultEnv
+    });
+  }
+
+  return triggerDeployScript({isCi: false});
 }
 
 startScript();
